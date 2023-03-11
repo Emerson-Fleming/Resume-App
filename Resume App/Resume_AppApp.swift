f9f63ae51e1cc550b3ae7ee4fe1410ac6e8bdd66
Emerson-Fleming <85723483+Emerson-Fleming@users.noreply.github.com>
2023-03-10 17:04:45 -0800

    Initial Commit

---
+++ Resume App/Resume_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resume_AppApp.swift
+//  Resume App
+//
+//  Created by Emerson Fleming on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Resume_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
